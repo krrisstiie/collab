d070615cddcc85f96c410444c7b0c7996d2605f2
krrisstiie <kristiefongg@gmail.com>
2024-06-25 10:10:23 -0700

    Added a message

--- collab/ContentView.swift
+++ collab/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, world! I'm Kristie and you're Hazel! 🩷")
         }
         .padding()
     }
