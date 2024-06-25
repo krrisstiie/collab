0bd64253577c4d29ef101578acf1edf36939ae8b
krrisstiie <kristiefongg@gmail.com>
2024-06-25 10:03:22 -0700

    Initial Commit

---
+++ collab/collabApp.swift
@@ -0,0 +1,17 @@
+//
+//  collabApp.swift
+//  collab
+//
+//  Created by Scholar on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct collabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
