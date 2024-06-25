60953b2b3ffb444e9a03fdb91779b53b06c88547
hazeljoyc <hazeljgchavez@gmail.com>
2024-06-25 14:01:22 -0700

    added gradient background

--- collab/ContentView.swift
+++ collab/ContentView.swift
@@ -9,20 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image("Katpng")
-                .resizable(resizingMode: .stretch)
-                .frame(width: /*@START_MENU_TOKEN@*/160.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/130.0/*@END_MENU_TOKEN@*/)
-                .foregroundStyle(.tint)
-            Text("EMEOWTION")
-                .font(.title)
-                .fontWeight(.bold)
-                .multilineTextAlignment(.center)
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.purple]),
+            startPoint: .topLeading, endPoint: .bottomTrailing)
+            .ignoresSafeArea(.all, edges: .all)
+            VStack {
+                Image("Katpng")
+                    .resizable(resizingMode: .stretch)
+                    .frame(width: /*@START_MENU_TOKEN@*/160.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/130.0/*@END_MENU_TOKEN@*/)
+                    .foregroundStyle(.tint)
+                Text("EMEOWTION")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+            }
         }
-        .padding()
     }
+    
 }
 
 #Preview {
     ContentView()
 }
+
