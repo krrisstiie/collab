1ede24f2d849d83c24a6bab58ee311950eae99e8
hazeljoyc <hazeljgchavez@gmail.com>
2024-06-26 12:38:37 -0700

    added start button

--- collab/ContentView.swift
+++ collab/ContentView.swift
@@ -9,20 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            LinearGradient(gradient: Gradient(colors: [Color("SpaceBlue"), Color("Orange")]),
-                           startPoint: .topLeading, endPoint: .bottomLeading)
-            .ignoresSafeArea(.all, edges: .all)
-            VStack {
-                Image("Kat head")
-                    .resizable(resizingMode: .stretch)
-                    .frame(width: /*@START_MENU_TOKEN@*/160.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/130.0/*@END_MENU_TOKEN@*/)
-                    .foregroundStyle(.tint)
-                Text("EMEOWTION")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.white)
-                    .multilineTextAlignment(.center)
+        NavigationStack {
+            ZStack {
+                LinearGradient(gradient: Gradient(colors: [Color("SpaceBlue"), Color("Orange")]),
+                               startPoint: .topLeading, endPoint: .bottomLeading)
+                .ignoresSafeArea(.all, edges: .all)
+                VStack {
+                    Image("Kat head")
+                        .resizable(resizingMode: .stretch)
+                        .frame(width: /*@START_MENU_TOKEN@*/160.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/130.0/*@END_MENU_TOKEN@*/)
+                        .foregroundStyle(.tint)
+                    Text("EMEOWTION")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.center)
+
+
+                    NavigationLink(destination: DailyCheckIn()) {
+                        Text("Start")
+                            .font(.title)
+                            .fontWeight(.medium)
+                            .foregroundColor(Color.white)
+                                .padding()
+                            }
+                            .font(.largeTitle)
+                            .buttonStyle(.borderedProminent)
+                            .tint(Color(red: 1.0, green: 1.0, blue: 1.0, opacity: 0.2))
+                }
             }
         }
     }
