eeb3f140a1f34f6c4415c69324cbe11d7734af1e
hazeljoyc <hazeljgchavez@gmail.com>
2024-06-26 13:52:37 -0700

    added background

--- collab/DailyCheckIn.swift
+++ collab/DailyCheckIn.swift
@@ -13,6 +13,12 @@ struct DailyCheckIn: View {
             LinearGradient(gradient: Gradient(colors: [Color("LightBlue"), Color("OrangeCream")]),
                            startPoint: .topLeading, endPoint: .bottomLeading)
             .ignoresSafeArea(.all, edges: .all)
+            Image("Stars")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                .opacity(0.5)
+            
         }
     }
 }
