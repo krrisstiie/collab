a24496cb3db4ee55f4fb7a65b792524071184f29
hazeljoyc <hazeljgchavez@gmail.com>
2024-06-27 09:22:14 -0700

    added to checkinquestion

--- collab/CheckInQuestion.swift
+++ collab/CheckInQuestion.swift
@@ -10,17 +10,23 @@ import SwiftUI
 struct CheckInQuestion: View {
     var body: some View {
         VStack {
-            HStack(spacing: -20) {
+            HStack(spacing: -60) {
                 Image("Katpng")
                     .resizable()
-                    .frame(width: 150, height: 150)
+                    .frame(width: 180, height: 150)
+                    .padding(.trailing, 30.0)
                 Text("Why do you feel this way?")
+                    .multilineTextAlignment(.leading)
                     .padding(.trailing, 15.0)
                 
             }
+            .padding(.top, 5.0)
 
-            TextField("Type response here...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
-                .padding(.leading, 123.0)
+            TextField("Type response here...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/, axis: .vertical)
+                .lineLimit(6, reservesSpace: true)
+                .background(Color(.systemGroupedBackground))
+                .cornerRadius(15)
+                .padding()
         }
     }
 }
